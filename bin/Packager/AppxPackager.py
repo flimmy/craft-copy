d0caef5b55f0f99889d51e6e8fd375b9aeddb064
Michael 'Flimmy' Flemming <m.flemming@opencloud.eu>
2026-07-27 13:03:28 +0200

    make appx-Package-Type configurable, for msix support, MakeAppxExe uses the fileEnding to switch its internal logic

--- bin/Packager/AppxPackager.py
+++ bin/Packager/AppxPackager.py
@@ -121,6 +121,10 @@ class AppxPackager(CollectionPackagerBase):
             defines.setdefault("file_types", "")
             defines.setdefault("extensions", "")
 
+    @property
+    def _packageType(self) -> str:
+        return CraftCore.settings.get("Packager", "AppxPackageType", "appx")
+
     def setDefaults(self, defines: dict) -> dict:
         defines = super().setDefaults(defines)
         defines.setdefault("additional_xmlns", "")
@@ -149,7 +153,7 @@ class AppxPackager(CollectionPackagerBase):
         defines.setdefault("appx_identity_name", defines["name"])
 
         utils.createDir(self.artifactsDir())
-        defines["setupname"] = self.artifactsDir() / os.path.basename(f"{defines['setupname']}.msix")
+        defines["setupname"] = self.artifactsDir() / os.path.basename(f"{defines['setupname']}.{self._packageType}")
         defines.setdefault("craft_id", self.package.path.replace("/", "."))
 
         self._setupFileTypes(defines)
@@ -299,14 +303,14 @@ class AppxPackager(CollectionPackagerBase):
                 and self.symbolsImageDir().exists()
                 and any(self.symbolsImageDir().iterdir())
             ):
-                appxSym = Path(defines["setupname"]).with_suffix(".msixsym")
+                appxSym = Path(defines["setupname"]).with_suffix(f".{self._packageType}sym")
                 artifacts += [appxSym]
                 if appxSym.exists():
                     appxSym.unlink()
                 if not utils.compress(appxSym, self.archiveDebugDir()):
                     return False
 
-            appxUpload = (Path(self.packageDestinationDir()) / os.path.basename(defines["setupname"])).with_suffix(".msixupload")
+            appxUpload = (Path(self.packageDestinationDir()) / os.path.basename(defines["setupname"])).with_suffix(f".{self._packageType}upload")
             if appxUpload.exists():
                 appxUpload.unlink()
             if not utils.compress(appxUpload, artifacts):
